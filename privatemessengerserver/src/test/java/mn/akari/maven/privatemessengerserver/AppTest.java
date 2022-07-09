92a79b8af9aab17cff27077437410e5cc68fa6fb
Akari Takahashi <akaritakahashioss@gmail.com>
2022-07-09 16:06:26 +0900

    Add maven project

---
+++ privatemessengerserver/src/test/java/mn/akari/maven/privatemessengerserver/AppTest.java
@@ -0,0 +1,20 @@
+package mn.akari.maven.privatemessengerserver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
