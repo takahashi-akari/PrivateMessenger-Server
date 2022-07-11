f12023b5173577212089b340937273c6b1b3be87
Akari Takahashi <akaritakahashioss@gmail.com>
2022-07-11 16:58:57 +0900

    v0.0.12

--- privatemessengerserver/src/main/java/mn/akari/maven/privatemessengerserver/App.java
+++ privatemessengerserver/src/main/java/mn/akari/maven/privatemessengerserver/App.java
@@ -1,5 +1,5 @@
 // @title Private Messenger Server - App
-// @version 0.0.10
+// @version 0.0.12
 // @author Takahashi Akari <akaritakahashioss@gmail.com>
 // @date 2022-07-09
 // @description This is a private messenger server. App.java contains main method.
